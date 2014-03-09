a8a878d54a05d494fb6a3bd51f7db2b7b6522076
Jack Howland <jack.howl@gmail.com>
2014-03-08 23:30:11 -0800

    working on the crunching

--- lib/bb_stats_crunch.rb
+++ lib/bb_stats_crunch.rb
@@ -14,11 +14,28 @@ class BbStatsCrunch
     "#{name} is always HAPPY!!"
   end
 
-module GetStats
-  @stats = {}
-  CSV.foreach("files/GL1969.TXT") do |row|
-    puts row
-  end
-end
-end
+hometeam = 0
+visteam = 0
 
+module GetStats
+@hometeam = 0
+@visteam = 0
+  CSV.foreach("files/GL1968.TXT") do |row|
+    team1 = row[3]
+    score1 = row[9].to_i
+    team2 = row[6]
+    score2 = row[10].to_i
+      if score1 > score2
+    puts "The visitors  won #{team1} #{score1} #{team2} #{score2}"
+    @visteam += 1
+      else
+    puts "The home team won #{team2} #{score2} #{team1} #{score1}"
+    @hometeam += 1
+end
+end
+ #   puts "the home team won" @hometeam
+ #   puts "the visiting team won" @visteam
+ #   printf "hello" @visteam
+ #    puts "#{hometeam}"
+end
+end
